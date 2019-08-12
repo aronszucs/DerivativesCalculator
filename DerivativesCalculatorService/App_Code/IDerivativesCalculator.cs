5afb0372e46f6f342b4660576b40249fd53ce98e
aronszucs <szucsaron@hotmail.com>
2019-08-12 10:56:30 +0200

    upload

---
+++ DerivativesCalculatorService/App_Code/IDerivativesCalculator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+
+namespace DerivativesCalculatorService
+{
+    [ServiceContract]
+    public interface IDerivativesCalculator
+    {
+        [OperationContract]
+        Decimal CalculateDerivative(int days, string[] symbols, string[] functions);
+    }
+}
---
+++ Host/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DerivativesCalculatorService;
+using System;
+using System.ServiceModel;
+
+namespace Host
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Type serviceType = typeof(Calculator);
+            using (ServiceHost host = new ServiceHost(serviceType))
+            {
+                host.Open();
+                Console.WriteLine("The derivatives calculator seervice is available.");
+                Console.ReadLine();
+            }
+        }
+
+    }
+}
